e9d5e760d988512b25adcbb5a8922fc1c14d7e70
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-06-06 19:24:13 +0200

    adding some sort of simplistic game board

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+
 @Controller
 public class HomeController {
+
+    private String[][] list;
 
 //    @GetMapping(value = {"/", "/home"})
 //    public static ModelAndView homePage(){
@@ -19,19 +24,78 @@ public class HomeController {
 //    }
     @GetMapping(value = {"/", "/home"})
 //    @RequestMapping(value = {"/", "/home"})
-    public static String homePage(){
+    public String homePage(){
         System.out.println("*********");
+        list = new String[3][3];
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                list[i][j] = " ";
+            }
+        }
         return "index";
     }
 
     @PostMapping(value = {"/", "/home"})
-    public static String game(@RequestParam("button") String buttonValue){
+    public String game(@RequestParam("button") String buttonValue, Model model){
         System.out.println("**** post *****");
         if (buttonValue.equals("newGameButton")){
             System.out.println("new game pressed");
+            for (int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                    list[i][j] = " ";
+                }
+            }
         }else {
-            System.out.println("game button pressed");
+            System.out.println("game button pressed: ");
+            System.out.println(buttonValue);
+//            model.addAttribute(buttonValue, )
+            if (buttonValue.equals("button00")){
+                if (list[0][0].equals(" ")){
+                    list[0][0] = "x";
+                }
+            }else if (buttonValue.equals("button01")){
+                if (list[0][1].equals(" ")){
+                    list[0][1] = "x";
+                }
+            }else if (buttonValue.equals("button02")){
+                if (list[0][2].equals(" ")){
+                    list[0][2] = "x";
+                }
+            }else if (buttonValue.equals("button10")){
+                if (list[1][0].equals(" ")){
+                    list[1][0] = "o";
+                }
+            }else if (buttonValue.equals("button11")){
+                if (list[1][1].equals(" ")){
+                    list[1][1] = "o";
+                }
+            }else if (buttonValue.equals("button12")){
+                if (list[1][2].equals(" ")){
+                    list[1][2] = "o";
+                }
+            }else if (buttonValue.equals("button20")){
+                if (list[2][0].equals(" ")){
+                    list[2][0] = "x";
+                }
+            }else if (buttonValue.equals("button21")){
+                if (list[2][1].equals(" ")){
+                    list[2][1] = "x";
+                }
+            }else if (buttonValue.equals("button22")){
+                if (list[2][2].equals(" ")){
+                    list[2][2] = "x";
+                }
+            }
         }
+        model.addAttribute("list", list);
+        System.out.println("-------------------");
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                System.out.print("|" + list[i][j] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------");
         return "index";
     }
 
---
+++ src/main/resources/static/scripts/functions.js
@@ -0,0 +1,15 @@
+function testFun(clickedId, buttonName){
+//    var yourUrl = "http://localhost:8080/home";
+    console.log("czesc młocie");
+    alert(clickedId);
+//    document.write("something");
+//    document.getElementById(clickedId).disabled = true;
+//    alert(clickedId);
+//    var xhr = new XMLHttpRequest();
+//    xhr.open("POST", yourUrl, true);
+//    xhr.setRequestHeader('Content-Type', 'application/json');
+//    xhr.send(JSON.stringify({
+//        value: document.getElementById(clickedId).name
+//    }));
+//    alert(document.getElementById(clickedId).name);
+}
