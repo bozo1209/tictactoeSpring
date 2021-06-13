69d5b2af0b4c903fa612019083be8fddd6bf974b
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-06-13 19:18:39 +0200

    little html adjustment

--- src/main/resources/static/scripts/functions.js
+++ src/main/resources/static/scripts/functions.js
@@ -32,21 +32,143 @@ document.addEventListener("DOMContentLoaded", function() {
 //    src.appendChild(img);
 //    alert("${list[0][0]}")
 
+//    var emptyImg = document.createElement("img");
+//    emptyImg.src = "/images/empty.png";
+//    var xImg = document.createElement("img");
+//    xImg.src = "/images/x.png";
+//    var oImg = document.createElement("img");
+//    oImg.src = "/images/o.png";
 
-    var but00 = document.getElementById("button00").textContent;
-    var but01 = document.getElementById("button01").textContent;
-    var but02 = document.getElementById("button02").textContent;
-    var but10 = document.getElementById("button10").textContent;
-    var but11 = document.getElementById("button11").textContent;
-    var but12 = document.getElementById("button12").textContent;
-    var but20 = document.getElementById("button20").textContent;
-    var but21 = document.getElementById("button21").textContent;
-    var but22 = document.getElementById("button22").textContent;
+//    var emptySrc = document.getElementById("button00");
+//    emptySrc.appendChild(emptyImg);
 
-    alert("but00: " + but00 + " but01: " + but01 + " but02: " + but02 + "\n" + "but10: " + but10 + " but11: " + but11 + " but12: " + but12 + "\n" + "but20: " + but20 + " but21: " + but21 + " but22: " + but22);
-    if(but00 == " "){
-        alert("hhhhhhh")
-    }
-
+//    var but00 = document.getElementById("button00").textContent;
+//    var but01 = document.getElementById("button01").textContent;
+//    var but02 = document.getElementById("button02").textContent;
+//    var but10 = document.getElementById("button10").textContent;
+//    var but11 = document.getElementById("button11").textContent;
+//    var but12 = document.getElementById("button12").textContent;
+//    var but20 = document.getElementById("button20").textContent;
+//    var but21 = document.getElementById("button21").textContent;
+//    var but22 = document.getElementById("button22").textContent;
+//
+//    alert("but00: " + but00 + " but01: " + but01 + " but02: " + but02 + "\n" + "but10: " + but10 + " but11: " + but11 + " but12: " + but12 + "\n" + "but20: " + but20 + " but21: " + but21 + " but22: " + but22);
+//    if(but00 == " "){
+////        alert("1");
+//        var src = document.getElementById("button00");
+//        src.appendChild(emptyImg);
+//    }else if(but00 == "x"){
+////        alert("2");
+//        var src = document.getElementById("button00");
+//        src.appendChild(xImg);
+//    }else if(but00 == "o"){
+////        alert("3");
+//        var src = document.getElementById("button00");
+//        src.appendChild(oImg);
+//    }
+//    if(but01 == " "){
+////        alert("4");
+//        var src = document.getElementById("button01");
+//        src.appendChild(emptyImg);
+//    }else if(but01 == "x"){
+////        alert("5");
+//        var src = document.getElementById("button01");
+//        src.appendChild(xImg);
+//    }else if(but01 == "o"){
+////        alert("6");
+//        var src = document.getElementById("button01");
+//        src.appendChild(oImg);
+//    }
+//    if(but02 == " "){
+////            alert("1");
+//        var src = document.getElementById("button02");
+//        src.appendChild(emptyImg);
+//    }else if(but02 == "x"){
+////            alert("2");
+//         var src = document.getElementById("button02");
+//         src.appendChild(xImg);
+//    }else if(but02 == "o"){
+////            alert("3");
+//         var src = document.getElementById("button02");
+//         src.appendChild(oImg);
+//    }
+//        if(but10 == " "){
+////            alert("4");
+//         var src = document.getElementById("button10");
+//         src.appendChild(emptyImg);
+//    }else if(but10 == "x"){
+////            alert("5");
+//         var src = document.getElementById("button10");
+//         src.appendChild(xImg);
+//    }else if(but10 == "o"){
+////            alert("6");
+//         var src = document.getElementById("button10");
+//         src.appendChild(oImg);
+//    }
+//    if(but11 == " "){
+////            alert("1");
+//        var src = document.getElementById("button11");
+//        src.appendChild(emptyImg);
+//    }else if(but11 == "x"){
+////            alert("2");
+//        var src = document.getElementById("button11");
+//        src.appendChild(xImg);
+//    }else if(but11 == "o"){
+////            alert("3");
+//        var src = document.getElementById("button11");
+//        src.appendChild(oImg);
+//    }
+//    if(but12 == " "){
+////            alert("4");
+//        var src = document.getElementById("button12");
+//        src.appendChild(emptyImg);
+//    }else if(but12 == "x"){
+////            alert("5");
+//        var src = document.getElementById("button12");
+//        src.appendChild(xImg);
+//    }else if(but12 == "o"){
+////            alert("6");
+//        var src = document.getElementById("button12");
+//        src.appendChild(oImg);
+//    }
+//    if(but20 == " "){
+//        //            alert("1");
+//        var src = document.getElementById("button20");
+//        src.appendChild(emptyImg);
+//    }else if(but20 == "x"){
+//        //            alert("2");
+//        var src = document.getElementById("button20");
+//        src.appendChild(xImg);
+//    }else if(but20 == "o"){
+//        //            alert("3");
+//        var src = document.getElementById("button00");
+//        src.appendChild(oImg);
+//    }
+//    if(but21 == " "){
+//        //            alert("4");
+//        var src = document.getElementById("button21");
+//        src.appendChild(emptyImg);
+//    }else if(but21 == "x"){
+//        //            alert("5");
+//        var src = document.getElementById("button21");
+//        src.appendChild(xImg);
+//    }else if(but21 == "o"){
+//        //            alert("6");
+//        var src = document.getElementById("button21");
+//        src.appendChild(oImg);
+//    }
+//    if(but22 == " "){
+//                        //            alert("4");
+//        var src = document.getElementById("button22");
+//        src.appendChild(emptyImg);
+//    }else if(but22 == "x"){
+//                        //            alert("5");
+//        var src = document.getElementById("button22");
+//        src.appendChild(xImg);
+//    }else if(but22 == "o"){
+//                        //            alert("6");
+//        var src = document.getElementById("button22");
+//        src.appendChild(oImg);
+//    }
 
 });
